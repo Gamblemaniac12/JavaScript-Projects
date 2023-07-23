ab8c3c5135bb6efeb7f788a53717d3fbba45d8c7
Caleb Gamble <135040498+Gamblemaniac12@users.noreply.github.com>
2023-07-23 03:27:01 -0700

    Add files via upload

--- Project 13_Pizza_App/js/pizza.js
+++ Project 13_Pizza_App/js/pizza.js
@@ -1,108 +1,133 @@
-function Total() {
-	var list; //for itemizing receipt
-	var totalPrice = 0;
+
+function getOrder() {
+	var txt = "<h3>You Ordered:</h3>";
+	var txt2 = " ";
+	var runningTotal = 0;
+	var sizeTotal = 0;
+	var sizeArray = document.getElementsByClassName("size");
+	for (var i = 0; i < sizeArray.length; i++) {
+		if (sizeArray[i].checked) {
+			var selectedSize = sizeArray[i].value;
+			txt += selectedSize+"<br>";
+		}
+	}
+	if (selectedSize === "Personal Pizza") {
+		sizeTotal = 6;
+	} else if (selectedSize === "Medium Pizza") {
+		sizeTotal = 10;
+	} else if (selectedSize === "Large Pizza") {
+		sizeTotal = 14;
+	} else if (selectedSize === "Extra Large Pizza") {
+		sizeTotal = 16;
+	}
+	runningTotal = sizeTotal;
+	txt2 = " <br>"+"<i> Size: </i>"+ "$"+sizeTotal + txt2;
+	getMeat(runningTotal,txt,txt2); 
+	};
 	
-	//Size
-	var sizePrice = 0;
-	var sizes = document.getElementsByClassName("size"); 
-
-	for (var i=0; i<sizes.length; i++) {
-		if (sizes[i].checked) {
-			var selSize = sizes[i].value;
+	function getMeat(runningTotal,txt,txt2) {
+	var meatTotal = 0;
+	var selectedMeat = [];
+	var meatArray = document.getElementsByClassName("meats");
+	for (var j = 0; j < meatArray.length; j++) {
+		if (meatArray[j].checked) {
+			selectedMeat.push(meatArray[j].value);
+			txt += meatArray[j].value+"<br>";
 		}
 	}
-
-	if (selSize === "Personal Size Pizza") {
-		sizePrice = 6;
-	} else if (selSize === "Medium Pizza") {
-		sizePrice = 10;
-	} else if (selSize === "Large Pizza") {
-		sizePrice = 14;
-	} else if (selSize === "Extra Large Pizza") {
-		sizePrice = 16;
+	var meatCount = selectedMeat.length;
+	if (meatCount > 1) {
+		meatTotal = (meatCount - 1);
+	} else {
+		meatTotal = 0;
 	}
-
-	//Crust
-	var crusts = document.getElementsByClassName("crust"); 
-	for (var j=0; j<crusts.length; j++) {
-		if (crusts[j].checked) {
-			var selCrust = crusts[j].value;
+	runningTotal = (runningTotal + meatTotal);
+	txt2 = " <br>" + "<i> Meats: </i>"+ "$"+meatTotal + txt2 +"<br>";
+	getCheese(runningTotal,txt, txt2);
+	};
+	
+	function getCheese(runningTotal,txt,txt2) {
+	var cheeseTotal = 0;
+	var cheeseArray = document.getElementsByClassName("cheese");
+	for (var k = 0; k < cheeseArray.length; k++) {
+		if (cheeseArray[k].checked) {
+			var selectedCheese = cheeseArray[k].value;
+			txt += selectedCheese+"<br>";
+			} 
 		}
+	if (selectedCheese === "Extra Cheese") {
+		cheeseTotal = 3; 
+	} else {
+		cheeseTotal = 0;
 	}
-
-	//Sauce
-	var sauces = document.getElementsByClassName("sauce");
-	for (var k=0; k<sauces.length; k++) {
-		if (sauces[k].checked) {
-			var selSauce = sauces[k].value;
+	runningTotal = (runningTotal + cheeseTotal);
+	txt2 = " <br>" +" <i>Cheese:</i> "+ "$"+cheeseTotal + txt2 +"<br>" ;
+	getCrust(runningTotal,txt,txt2);
+	};
+	
+	function getCrust(runningTotal,txt,txt2) {
+	var crustTotal = 0;
+	var crustArray = document.getElementsByClassName("crust");
+	for (var c = 0; c < crustArray.length; c++) {
+		if (crustArray[c].checked) {
+			var selectedCrust = crustArray[c].value;
+			txt += selectedCrust+"<br>";
+			}
 		}
-	}
-
-//Cheese
-	var cheesePrice = 0;
-	var cheeses = document.getElementsByClassName("cheese");
-	for (var l=0; l<cheeses.length; l++) {
-		if (cheeses[l].checked) {
-			var selCheese = cheeses[l].value;
-		}
-	}
-		if (selCheese === "Extra Cheese") {
-		cheesePrice = 3;
+	if (selectedCrust === "Cheese Stuffed Crust") {
+		crustTotal = 3; 
 		} else {
-		cheesePrice = 0;
+			crustTotal = 0;
 		}
-
-	//Itemize current selections on receipt
-	list = selSize + "<br />" + selCrust + "<br />" + selSauce + "<br />" + selCheese + "<br />";
-
-	totalPrice = (sizePrice + cheesePrice);
-
-	Meats(totalPrice,list);
-};
-
-function Meats(totalPrice,list) {
-	var meatsPrice = 0;
-	var selectedMeat = []; //declares new array, recommended over using the new Array() constructor.
-	var meats = document.getElementsByClassName("meats");
-
-	for (var m=0; m<meats.length; m++) {
-		if (meats[m].checked) {
-			selectedMeat.push(meats[m].value);
-			list = list + meats[m].value + "<br />";
+	runningTotal = (runningTotal + crustTotal);
+	txt2 = " <br>" +" <i>Crust:</i> "+ "$"+crustTotal + txt2 +"<br>" ;
+	getSauce(runningTotal,txt,txt2);
+	};
+	
+	function getSauce(runningTotal,txt,txt2) {
+	var sauceTotal = 0;
+	var sauceArray = document.getElementsByClassName("sauce");
+	for (var s = 0; s < sauceArray.length; s++) {
+		if (sauceArray[s].checked) {
+			var selectedSauce = sauceArray[s].value;
+			txt += selectedSauce+"<br>";
 		}
 	}
-
-	var meatsCount = selectedMeat.length;
-	if (meatsCount>1) {
-		meatsPrice = (meatsCount - 1);
+	runningTotal = (runningTotal + sauceTotal);
+	txt2 = "<br>" + " <i>Sauce:</i> "+ "$"+sauceTotal + txt2 +"<br>" ;
+	getVeggie(runningTotal,txt,txt2);
+	};
+	
+	function getVeggie(runningTotal,txt,txt2) {
+	var veggieTotal = 0;
+	var selectedVeggie = [];
+	var veggieArray = document.getElementsByClassName("veggies");
+	for (var p = 0; p < veggieArray.length; p++) {
+		if (veggieArray[p].checked) {
+			selectedVeggie.push(veggieArray[p].value);
+			txt += veggieArray[p].value+"<br>";
+		}
+	}
+	var veggieCount = selectedVeggie.length;
+	if (veggieCount > 1) {
+		veggieTotal = (veggieCount - 1);
 	} else {
-		meatsPrice = 0;
+		veggieTotal = 0;
 	}
-	totalPrice = (totalPrice + meatsPrice);	
-	document.getElementById("invoiceText").innerHTML = list;
-	document.getElementById("totalPrice").innerHTML = "Your Total: $" + totalPrice + ".00";
-	Veggies(totalPrice,list);
-};
-
-function Veggies(totalPrice,list) {
-	var veggiesPrice = 0;
-	var selectedVeggies = []; 
-	var veggies = document.getElementsByClassName("veggies");
-
-	for (var n=0; n<veggies.length; n++) {
-		if (veggies[n].checked) {
-			selectedVeggies.push(veggies[n].value);
-			list = list + veggies[n].value + "<br />";
-		}
+	runningTotal = (runningTotal + veggieTotal);
+	txt2 = "<br>"+" <i>Veggies:</i> "+ "$"+veggieTotal + txt2 +"<br>" ;
+	
+	document.getElementById("displayTxt").innerHTML= txt;
+	document.getElementById("runningTotal2").innerHTML = "<h3>Running Total:</h3>" +txt2;
+	document.getElementById("totalPrice").innerHTML = "Total Price: $"+runningTotal+".00"+"</strong></h3>";
+	};
+	
+	function cancelOrder() {
+	var unCheck= document.getElementsByTagName("input");
+	for (var i = 0; i < unCheck.length; i++) {
+		unCheck[i].checked = false 
 	}
-
-	var veggiesCount = selectedVeggies.length;
-	if (veggiesCount>1) {
-		veggiesPrice = (veggiesCount - 1);
-	} else {
-		veggiesPrice = 0;
-	}
-	totalPrice = (totalPrice + veggiesPrice);	
-	document.getElementById("invoiceText").innerHTML = list;
-	document.getElementById("totalPrice").innerHTML = "Your Total: $" + totalPrice + ".00";
-};
+	document.getElementById("displayTxt").innerHTML = " "; 
+	document.getElementById("totalPrice").innerHTML = " ";
+	document.getElementById("runningTotal2").innerHTML = " ";
+	};
